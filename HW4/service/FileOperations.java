8432709bc1a614b831a9856b42ca8b4abe6e5c15
Olga <origanna@yandex.ru>
2025-07-09 14:34:13 +0300

    Add HW4

---
+++ HW4/service/FileOperations.java
@@ -0,0 +1,10 @@
+package HW4.service;
+
+import HW4.model.FamilyTree;
+import java.io.IOException;
+
+public interface FileOperations<T> {
+    void saveToFile(FamilyTree<T> familyTree, String fileName) throws IOException;
+
+    FamilyTree<T> loadFromFile(String fileName) throws IOException, ClassNotFoundException;
+}
